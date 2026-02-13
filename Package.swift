734794d42f94ac7e9305d443107ad30181bc18c2
zhangyan <zhyabs@gmail.com>
2026-02-13 21:13:28 +0800

    Update LibXray binary target URL and checksum

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LibXray",
-            url:"https://github.com/wanliyunyan/LibXray/releases/download/26.1.23/LibXray.xcframework.zip",
-            checksum: "9048e9ac0e5c2f1cb374950e0f30ebfe3d74e7e2424d0f9ca27e81e9baa91fc2"
+            url:"https://github.com/wanliyunyan/LibXray/releases/download/25.12.8/LibXray.xcframework.zip",
+            checksum: "c03aa77d25ba68e1038d112946d258a1bca432f06d154062457766531b4c8cd7"
         )
     ]
 )
